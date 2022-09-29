8b8d316e5a49f3ce00cf7e8b6eb6f177866449c6
John Smera <johnsmera@live.com>
2022-09-29 02:01:39 -0300

    feat: add Loading component

---
+++ app/components/Loading/Loading.tsx
@@ -0,0 +1,28 @@
+import { Progress, Spinner } from "@chakra-ui/react";
+
+type Props = {
+  loading: boolean;
+};
+export const Loading = ({ loading }: Props) => {
+  return (
+    <div
+      style={{
+        height: loading ? "100%" : 0,
+        width: loading ? "100%" : 0,
+        minHeight: loading ? "2.4rem" : 0,
+        marginLeft: loading ? "-0.8rem" : 0,
+        visibility: loading ? "visible" : "hidden",
+
+        opacity: "0.8",
+        zIndex: 2,
+        display: "flex",
+        flexDirection: "column",
+        gap: "1.2rem",
+        position: "absolute",
+        color: "#E2E0F5",
+      }}
+    >
+      <Progress zIndex={9} size="xs" isIndeterminate />
+    </div>
+  );
+};
